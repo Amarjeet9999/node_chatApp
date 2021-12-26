55f6db3b968730c010ae5aecdb45975802b1616d
Amaarjeet Kumar <amarjeetyt@gmail.com>
2021-12-26 13:40:42 +0530

    Adding Last Commit

--- public/client.js
+++ public/client.js
@@ -1,4 +1,7 @@
-const socket = io();
+const socket = io(); // Web socket allows us to communicate in realtime within the
+// client and the server
+
+// DOM elements
 const container = document.querySelector(".container");
 const sendForm = document.querySelector(".send");
 const input = document.querySelector("#input");
@@ -6,16 +9,27 @@ const usernameForm = document.getElementById("usernameForm");
 const enterUsername = document.getElementById("enterUsername");
 
 let userName; // Entered User
+let activeUsers = []; // active users
 
-let prevData = []; // for storing previous chat
-
+// Chat entering function || At first whenever the UI renders this
+// Will call and ask for name and fire a event new_user_joined.
 usernameForm.addEventListener("submit", (e) => {
   e.preventDefault();
   userName = enterUsername.value;
   document.querySelector(".nameBox").style.display = "none";
   socket.emit("new_user_joined", userName);
+  hideWelcome();
 });
 
+// very Simple Welcome Logic, UI part
+const hideWelcome = () => {
+  document.querySelector(".welcome").style.display = "block";
+  setTimeout(() => {
+    document.querySelector(".welcome").style.display = "none";
+  }, 3000);
+};
+
+// UI Logic for showing message on the UI.
 const append = (name, message, position) => {
   const mainDiv = document.createElement("div");
   const upper = document.createElement("upper");
@@ -30,6 +44,7 @@ const append = (name, message, position) => {
   container.append(mainDiv);
 };
 
+// UI Logic for whenever there will be a new user join
 const joinAppend = (message, position) => {
   const mainDiv = document.createElement("div");
   mainDiv.innerHTML = message;
@@ -38,6 +53,7 @@ const joinAppend = (message, position) => {
   container.append(mainDiv);
 };
 
+// Logic for send message
 sendForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const inputMessage = input.value;
@@ -47,14 +63,53 @@ sendForm.addEventListener("submit", (e) => {
   input.value = "";
 });
 
+// Whenever the user joined event will fire
 socket.on("user_joined", (name) => {
   joinAppend(`${name} Joined the chat`, "mid");
 });
 
+// When ever there will be recieve event fire
 socket.on("recieve", (data) => {
   append(data.name, data.message, "left");
 });
 
+// Calling user_left whenever any user left and a callback functions
+// which will append user left UI logic.
 socket.on("user_left", (data) => {
   joinAppend(`${data} Left the chat`, "mid");
+});
+
+// UI Logic for showing Active users
+let inner = document.querySelector(".inner-inner");
+const appendUsers = (e) => {
+  let main = document.createElement("div");
+  main.setAttribute("class", "mainList");
+  let div = document.createElement("div");
+  div.setAttribute("class", "usersList");
+  let span = document.createElement("span");
+  span.setAttribute("class", "spanOnline");
+  div.innerText = e;
+  main.append(span, div);
+  inner.append(main);
+};
+
+// Logic for opening active users list
+let open = document.querySelector(".activeUsers");
+open.addEventListener("click", () => {
+  socket.emit("send_users");
+  socket.on("get_users", (user) => {
+    activeUsers = [];
+    inner.innerHTML = null;
+    for (key in user) {
+      appendUsers(user[key]);
+      activeUsers.push(user[key]);
+    }
+  });
+  document.querySelector(".showAllusers").style.display = "block";
+});
+
+// Logic for closing active users list
+let close = document.querySelector(".close");
+close.addEventListener("click", () => {
+  document.querySelector(".showAllusers").style.display = "none";
 });
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+// Communication endpoint
 const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
@@ -14,17 +15,22 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+// Variable for storing user name with their ids
 const user = {};
 
+//
+
+// io.on creates a socket whenever there is a new user
 io.on("connection", (socket) => {
-  // User Joining Event
+  // Created a new_user_joined event which will first stores current user id and
+  // then it will will broadcast to others that someone have joined.
   socket.on("new_user_joined", (name) => {
     user[socket.id] = name;
     socket.broadcast.emit(`user_joined`, name);
-    // console.log(user);
   });
 
-  // For message event
+  // Create a send event which will which will calls in client side
+  // and it will broadcast the message to others.
   socket.on("send", (message) => {
     // console.log(message);
     socket.broadcast.emit("recieve", {
@@ -33,11 +39,24 @@ io.on("connection", (socket) => {
     });
   });
 
+  // For sending active users data
+  socket.on("send_users", () => {
+    socket.emit("get_users", user);
+  });
+
+  // Disconnect event returns username to the client.
   socket.on("disconnect", () => {
     socket.broadcast.emit("user_left", user[socket.id]);
+    removeUser(socket.id);
   });
 });
 
+// For Removing user from user object
+const removeUser = (el) => {
+  delete user[el];
+};
+
+// Create a HTTP server that will listen on PORT 5000 in the current computer
 module.exports = () => {
   try {
     http.listen(port, () => {
